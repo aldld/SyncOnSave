cc93a3a4e3ac81c87f4a11d22205e565b3a7570a
Eric Bannatyne <eric.bannatyne@gmail.com>
2015-11-22 15:18:34 -0500

    Convert tabs to spaces because it's nicer.

--- SyncOnSave.py
+++ SyncOnSave.py
@@ -8,24 +8,24 @@ DEBUG = False
 base_cmd = "rsync -arv {0} {1}"
 
 class SyncOnSaveCommand(sublime_plugin.EventListener):
-	def on_post_save(self, view):
-		""" Sync directory containing the current directory after saving. """
-		synced_dirs = sublime.load_settings("SyncOnSave.sublime-settings").get("synced_dirs")
+    def on_post_save(self, view):
+        """ Sync directory containing the current directory after saving. """
+        synced_dirs = sublime.load_settings("SyncOnSave.sublime-settings").get("synced_dirs")
 
-		file_name = view.file_name()
+        file_name = view.file_name()
 
-		for synced_dir in synced_dirs.keys():
-			if file_name.startswith(synced_dir):
-				# Current file is in a synced directory.
-				remote = synced_dirs[synced_dir]["remote"]
-				cmd = base_cmd.format(synced_dir, remote)
+        for synced_dir in synced_dirs.keys():
+            if file_name.startswith(synced_dir):
+                # Current file is in a synced directory.
+                remote = synced_dirs[synced_dir]["remote"]
+                cmd = base_cmd.format(synced_dir, remote)
 
-				if DEBUG:
-					print cmd
+                if DEBUG:
+                    print cmd
 
-				# Execute rsync in a new thread, to avoid noticeable network lag on save.
-				thread = threading.Thread(target=subprocess.call, args=(cmd,), kwargs={"shell": True})
-				thread.daemon = True
-				thread.start()
+                # Execute rsync in a new thread, to avoid noticeable network lag on save.
+                thread = threading.Thread(target=subprocess.call, args=(cmd,), kwargs={"shell": True})
+                thread.daemon = True
+                thread.start()
 
 
